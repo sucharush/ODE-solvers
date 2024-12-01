a89acac3c64ab884e385f1aa8fef34297f997c01
EB <estelle.baup@epfl.ch>
2024-12-01 11:43:44 +0100

    add utils.h

---
+++ include/utils.h
@@ -0,0 +1,27 @@
+/**
+ Created by ebaup on 01.12.24.
+ * @file utils.h
+ * @brief Utility functions, type definitions and constants for the project.
+ *
+ * This header file contains:
+ * - Commonly used type definitions (`typedef`) to simplify code readability.
+ * - General-purpose utility functions (helpers) used throughout the project, reusable in different contexts.
+ * - Constants variables shared across multiple modules.
+ *
+ * @author ebaup
+ * @date 01.12.24.
+ */
+
+#ifndef UTILS_H
+#define UTILS_H
+
+#include <functional>
+
+// Typedef for space variable type (y)
+typedef double var_y; //TODO: change to Eigen::VectorXf, do we need the dimension as parameter in RHS?
+
+// Typedef for function and derivative type
+typedef std::function<double(double, double)> FuncType;
+typedef std::function<double(double, double)> DerivativeType; // TODO: do we use it?
+
+#endif //UTILS_H
